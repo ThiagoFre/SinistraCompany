efa9f379caf4f5d438adb5f67842fccb730c9914
Push <neizferrmob@gmail.com>
2022-05-26 07:32:08 -0300

    primeiraVersãoDoCódigo

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const form = document.getElementById('form');
+const nome = document.getElementById('nome');
+const descricao = document.getElementById('descricao');
+const url = document.getElementById('url');
+
+form.addEventListener('submit', (e) => {
+
+
+
+
+
+
+
+
+
+})
